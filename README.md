60c973b2fbd3d25c386e49d237b75ac8d15aa904
unknown <jidapa.angsutti@ENGINEERING.SNOW>
2025-09-08 22:36:01 -0600

    simple arrays of words in each document

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 To keep things simple:
 
-Each word is defined by separation of white space or any punctuation.
+Each "word" is defined by separation of white space or any of the following punctuations: . , -.
+(I chose these by literally CTRL-F'ing the mission statements for all punctuations on the regular keyboard. You can see 'aggregate.txt' of the remnants of that process)
 
 Thus, "Tech" and "Tech's" will be recognized as different words
 
--- data.py
+++ data.py
@@ -1,9 +1,16 @@
-# gatech = open("./data/gatech.txt", "r", encoding='utf-8')
-# print(gatech.read())
-# print(type(gatech))
+import re
+import os
 
-with open('./data/gatech.txt', 'r', encoding='utf-8') as file:
-    content = file.read()
-    print(content)
-    print(content[2])
-    print(type(content))
+root_dir = os.getcwd()
+data_dir = root_dir + (r"\data")
+print(data_dir)
+
+delimiting_pattern = r'[ .,-]'
+for file_name in os.listdir(data_dir):
+    # print(f"Reading {file_name}: ")
+    full_path = data_dir + '\\' + file_name
+    with open(full_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+        split_results = re.split(delimiting_pattern, content)
+        words = [r for r in split_results if r.strip()]
+        print(words)
